35bb590a489d8cd0d1e837def0e6a01a2ca4f982
VenSayn <burdenko_789@mail.ru>
2025-09-06 17:36:44 +0300

    Домашнее задание 6

---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import React, { useMemo } from 'react';
+
+const TodoList = ({ todos }) => {
+
+// Оптимизация списка задач
+
+const renderedTodos = useMemo(() => {
+
+return todos.map((todo, index) => (
+
+<div key={index} className={`todo-item ${todo.completed ? 'completed' : 
+''}`}>
+
+<span>{todo.title}</span>
+
+</div>
+
+));
+
+}, [todos]);
+
+
+return <div className="todo-list">{renderedTodos}</div>;
+
+};
+
+
+export default TodoList;
+
